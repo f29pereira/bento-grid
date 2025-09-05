307967115edd869da41a57fcac4f6dc811148bdd
Filipe Pereira <f29.pereira@gmail.com>
2025-09-05 20:39:04 +0100

    Update styling, Add font, Add SocialMedia component
    - Update global.css styling
    - Add DM Sans Variable Font to layout.tsx
    - Add SocialMedia component and SocialMedia.module.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
+
+export const dmSans = localFont({
+  src: "./fonts/DMSans-VariableFont_opsz,wght.ttf",
+  variable: "--font-DMSans",
+});
+
+export const dmSansItalic = localFont({
+  src: "./fonts/DMSans-Italic-VariableFont_opsz,wght.ttf",
+  variable: "--font-DMSansItalic",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${dmSans.variable} ${dmSansItalic.variable}`}>
+      <title>Bento Grid</title>
       <body>{children}</body>
     </html>
   );
