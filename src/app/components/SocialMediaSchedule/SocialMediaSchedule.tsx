15a47349b41be01d93ea8cd22686c03527128713
Filipe Pereira <f29.pereira@gmail.com>
2025-09-06 12:33:45 +0100

    Add SocialMedia component
    - Add SocialMedia component and SocialMedia.module.css

---
+++ src/app/components/SocialMediaSchedule/SocialMediaSchedule.tsx
@@ -0,0 +1,23 @@
+import styles from "./SocialMedia.module.css";
+import Image from "next/image";
+
+export default function SocialMediaSchedule() {
+  return (
+    <div className={`flex-col-center border-r-10 softPurpleBg ${styles.card}`}>
+      <h1 className={`${styles.centerText} ${styles.title}`}>
+        Schedule to social media.
+      </h1>
+      <Image
+        src="/images/illustration-schedule-posts.webp"
+        alt="Schedule post graph showing the best day to post"
+        width={715}
+        height={638}
+        className={styles.img}
+      />
+      <p className={`${styles.centerText} ${styles.text}`}>
+        Optimize post timings to publish content at the perfect time for your
+        audience.
+      </p>
+    </div>
+  );
+}
