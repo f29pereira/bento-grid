ff58009cab7acc1645e68a2ec2873f78835da82d
Filipe Pereira <f29.pereira@gmail.com>
2025-09-06 15:20:58 +0100

    Add WriteContent component
    - Add WriteContent component and WriteContent.module.css

---
+++ src/app/components/WriteContent/WriteContent.tsx
@@ -0,0 +1,20 @@
+import styles from "./WriteContent.module.css";
+import Image from "next/image";
+
+/**
+ * Renders write content with AI card
+ */
+export default function WriteContent() {
+  return (
+    <div className={`yellowBg border-r-10 ${styles.card}`}>
+      <h1 className={styles.title}>Write your content using AI.</h1>
+      <Image
+        src="/images/illustration-ai-content.webp"
+        alt="Post button with arrow"
+        width={440}
+        height={446}
+        className={styles.img}
+      />
+    </div>
+  );
+}
